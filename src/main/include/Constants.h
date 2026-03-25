e619e9b728214d259e6174a4131a7c635a180eb3
RoboRaiders3691 <northfieldfrc3691@gmail.com>
2026-03-24 20:32:16 -0500

    updated intake roller constants

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -42,11 +42,11 @@ namespace IntakeConstants {
     inline constexpr int kCanIDCANCoder = 5;
 
     inline constexpr int kCanIDPivot = 21;
-    inline constexpr units::angle::turn_t kPivotUpperLimit = .36_tr;
-    inline constexpr units::angle::turn_t kPivotLowerLimit = -.07_tr;
+    inline constexpr units::angle::turn_t kPivotUpperLimit = .45_tr;
+    inline constexpr units::angle::turn_t kPivotLowerLimit = 0_tr;
 
     inline constexpr int kCanIDRoller = 20;
-    inline constexpr units::turns_per_second_t kIntakeVelocityConstant = -65_tps;
+    inline constexpr units::turns_per_second_t kIntakeVelocityConstant = -60_tps;
 
     static constexpr ctre::phoenix6::configs::CANcoderConfiguration kIntakeCANCoderConfigs = ctre::phoenix6::configs::CANcoderConfiguration{}
         .WithMagnetSensor(ctre::phoenix6::configs::MagnetSensorConfigs{}
@@ -84,13 +84,13 @@ namespace IntakeConstants {
     static constexpr ctre::phoenix6::configs::TalonFXConfiguration KRollerConfigs = ctre::phoenix6::configs::TalonFXConfiguration{}
         .WithSlot0(ctre::phoenix6::configs::Slot0Configs{}
                     
-            .WithKP(5)
+            .WithKP(0)
             .WithKI(0)
-            .WithKD(.1)
+            .WithKD(0)
 
-            .WithKS(0.4)
-            .WithKV(0.15)
-            .WithKA(0)
+            .WithKS(0.349609375)
+            .WithKV(0.15000000596046448)
+            .WithKA(0.05)
             .WithKG(0)
             .WithGravityType(ctre::phoenix6::signals::GravityTypeValue::Arm_Cosine)
         )
